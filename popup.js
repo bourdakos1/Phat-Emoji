365331fa57a527227e86476363ac2259700832a1
Nick Bourdakos <bourdakos1@gmail.com>
2017-08-04 15:50:54 -0700

    updates

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('send_emoji').addEventListener('click', function() {
         chrome.tabs.executeScript(null, {
-            file: "sendMessage.js"
+            code: "sendMessage('poop')"
         }, function() {
             // If you try and inject into an extensions page or the webstore/NTP you'll get an error
             if (chrome.runtime.lastError) {
