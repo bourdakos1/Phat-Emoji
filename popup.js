cbe3f08777043a91a55bc412357e38f0a19b992e
Nick Bourdakos <bourdakos1@gmail.com>
2017-08-04 10:45:42 -0700

    always run in background

--- popup.js
+++ popup.js
@@ -1,13 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    chrome.tabs.executeScript(null, {
-        file: "getPagesSource.js"
-    }, function() {
-        // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-        if (chrome.runtime.lastError) {
-            // tokenView.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-        }
-    });
-
     document.getElementById('send_emoji').addEventListener('click', function() {
         chrome.tabs.executeScript(null, {
             file: "sendMessage.js"
