e63b8c93868449764c7c30b976fa841875dbc24d
Nick Bourdakos <bourdakos1@gmail.com>
2017-08-04 09:49:46 -0700

    separate page refresh and emoji sending

--- popup.js
+++ popup.js
@@ -7,4 +7,15 @@ document.addEventListener('DOMContentLoaded', function() {
             // tokenView.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
         }
     });
+
+    document.getElementById('send_emoji').addEventListener('click', function() {
+        chrome.tabs.executeScript(null, {
+            file: "sendMessage.js"
+        }, function() {
+            // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+            if (chrome.runtime.lastError) {
+                // tokenView.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+            }
+        });
+    });
 });
